1ef99801214aae2cde9ed9af6d22132b6bddca71
daeroworld <daeroworld@gmail.com>
2025-11-15 20:16:18 +0900

    🎨REFACTOR(model): Rename `ID` to `Id` member name

--- internal/api/domain/model/segment.go
+++ internal/api/domain/model/segment.go
@@ -1,7 +1,7 @@
 package model
 
 type Segment struct {
-	ID    int     `json:"id"`
+	Id    int     `json:"id"`
 	Start float64 `json:"start"`
 	End   float64 `json:"end"`
 	Text  string  `json:"text"`
